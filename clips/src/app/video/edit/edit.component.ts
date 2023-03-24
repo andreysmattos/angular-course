24e5f8ae7b72aeb3f005adc4ed1630c839e4308f
Andrey Mattos <andreysmattos@hotmail.com>
2023-03-24 20:17:43 -0300

    feat() bind fileName on Input on change active clip

--- clips/src/app/video/edit/edit.component.ts
+++ clips/src/app/video/edit/edit.component.ts
@@ -1,22 +1,51 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, OnChanges } from '@angular/core';
 import IClip from 'src/app/models/clips.model';
 import { ModalService } from 'src/app/services/modal.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent implements OnInit, OnDestroy{
+export class EditComponent implements OnInit, OnDestroy, OnChanges {
 
   @Input() activeClip: IClip | null = null;
+
+  clipId = new FormControl('', {
+    validators: [Validators.required],
+    nonNullable: true
+  });
+
+  title = new FormControl('', {
+    validators: [Validators.required, Validators.minLength(3)],
+    nonNullable: true
+  });
+
+  editForm: FormGroup = new FormGroup({
+    title: this.title,
+    id: this.clipId
+  });
 
   constructor(public modal: ModalService) {
 
   }
 
+  ngOnChanges() {
+    console.log('teste123xxxxxxxxxxxxx')
+
+    console.log('this.activeClip', this.activeClip);
+    if (!this.activeClip) {
+      return;
+    }
+
+
+    this.clipId.setValue(this.activeClip.uid);
+    this.title.setValue(this.activeClip.fileName)
+
+  }
+
   ngOnInit() {
-    console.log('teste123')
     this.modal.register('editClip');
   }
 
