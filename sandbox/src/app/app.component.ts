798a933f454f2239c55c3de3a9f8074bf0024eb1
Andrey Mattos <andreysmattos@hotmail.com>
2023-03-27 19:08:59 -0300

    feat()

--- sandbox/src/app/app.component.ts
+++ sandbox/src/app/app.component.ts
@@ -1,28 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-import { catchError, combineLatest, debounceTime, delay, EMPTY, forkJoin, fromEvent, interval, map, of, take, tap, timeInterval, timer, withLatestFrom } from 'rxjs';
+import {
+  catchError,
+  combineLatest,
+  debounceTime,
+  delay,
+  EMPTY,
+  forkJoin,
+  fromEvent,
+  interval,
+  map,
+  of,
+  take,
+  tap,
+  timeInterval,
+  timer,
+  withLatestFrom,
+  Subject,
+  BehaviorSubject,
+  ReplaySubject
+} from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 @Component({
   selector: 'app-root',
   template: `
-  <h1> Teste </h1> 
-
-  <button id="btn1">btn1</button> <br />
-  <button id="btn2">btn2</button> <br />
-  <button id="btn3">btn3</button> <br />
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis dolore quasi, veritatis possimus totam dolores eligendi! Vero ipsa itaque aliquid, repellendus harum nisi cum tempore illo pariatur magnam non libero!
+  </p>
   `
 })
 export class AppComponent {
 
   constructor() {
+    const subject = new Subject();
+    const behaviorSubject = new BehaviorSubject(-1);
+
+    subject.next(0); // não vai mostrar
+    behaviorSubject.next(0);
+
+    subject.subscribe(value => console.log('Subject: ' + value))
+    behaviorSubject.subscribe(value => console.log('Behavior: ' + value))
+
+    subject.next(1);
+    behaviorSubject.next(1);
+
+    subject.next(2);
+    behaviorSubject.next(2);
+
+    const onlyObservable = behaviorSubject.asObservable(); 
+    // onlyObservable não pode usar o .next quando é usado o asObservable
+
+    behaviorSubject.next(123)
+
+
+    onlyObservable.subscribe(console.log)
 
   }
 
-  ngAfterViewInit() {
 
-    const second$ = interval(3000);
-    interval(1000).pipe(withLatestFrom(second$)).subscribe(console.log)
-
-  }
 
 }
