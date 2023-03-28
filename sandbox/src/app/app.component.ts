9367e7137d2e75ece58763a3a58408f187051fb3
Andrey Mattos <andreysmattos@hotmail.com>
2023-03-28 19:50:58 -0300

    example

--- sandbox/src/app/app.component.ts
+++ sandbox/src/app/app.component.ts
@@ -1,47 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import {
-  catchError,
-  combineLatest,
-  debounceTime,
-  delay,
-  EMPTY,
-  forkJoin,
-  fromEvent,
-  interval,
-  map,
-  of,
-  take,
   tap,
-  timeInterval,
-  timer,
-  withLatestFrom,
-  Subject,
   BehaviorSubject,
-  ReplaySubject,
-  startWith,
-  merge
+  shareReplay,
+  mergeMap
 } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-
 @Component({
   selector: 'app-root',
   template: `
-  <p>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis dolore quasi, veritatis possimus totam dolores eligendi! Vero ipsa itaque aliquid, repellendus harum nisi cum tempore illo pariatur magnam non libero!
-  </p>
+
+
+  <div *ngIf="!(loading$ | async) else loading">
+
+    <h1 *ngIf="products$ | async as products">{{products['first_name']}}</h1>
+    
+    <button (click)="update()">update</button>
+  </div>
+  
+  <ng-template #loading >
+    <div  style="width: 25px; height: 25px; margin: 15px; border-radius: 100%; border: solid 4px blue; border-left-color: transparent;"></div>
+    <small>... carregando</small>
+  </ng-template>
+
   `
 })
 export class AppComponent {
 
+  private refresh = new BehaviorSubject<void>(undefined);
+  public loading$ = new BehaviorSubject(true);
+
+  products$ = this.refresh.pipe(
+    tap(() => this.loading$.next(true)),
+    mergeMap(() => ajax.getJSON<any>('https://random-data-api.com/api/v2/users?size=1&is_xml=true')),
+    tap(() => this.loading$.next(false)),
+
+    shareReplay(1),
+  );
+
   constructor() {
-
-    const clicks = fromEvent(document!, 'click');
-    const timer = interval(1000);
-
-    merge(clicks, timer).subscribe(console.log);
+    // varios subscribe sem fazer requests
+    this.products$.subscribe(console.log)
+    this.products$.subscribe(console.log)
+    this.products$.subscribe(console.log)
+    this.products$.subscribe(console.log)
+    this.products$.subscribe(console.log)
 
   }
 
-
+  update() {
+    // faz request para atualizar os dados
+    // carrega o loading de forma automatica
+    this.refresh.next();
+  }
 
 }
