81c42a54717979e2fd2b667cf73d60fe205f9750
Andrey Mattos <andreysmattos@hotmail.com>
2023-03-29 20:35:08 -0300

    feat() switchMap, mergeMap and concatMap

--- rxjs/APM/src/app/suppliers/supplier.service.ts
+++ rxjs/APM/src/app/suppliers/supplier.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
-import { throwError, Observable } from 'rxjs';
+import { throwError, Observable, of, tap, concatMap, mergeMap, switchMap } from 'rxjs';
+import { Supplier } from './supplier';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,28 @@ import { throwError, Observable } from 'rxjs';
 export class SupplierService {
   suppliersUrl = 'api/suppliers';
 
-  constructor(private http: HttpClient) { }
+
+  supllierWithConcatMap$ = of(1, 5, 8).pipe(
+    tap(id => console.log('ConcatMap source Operator: ', id)),
+    concatMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+  );
+
+  supllierWithMergeMap$ = of(1, 5, 8).pipe(
+    tap(id => console.log('MergeMap source Operator: ', id)),
+    mergeMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+  );
+
+  supllierWithSwitchMap$ = of(1, 5, 8).pipe(
+    tap(id => console.log('SwitchMap source Operator: ', id)),
+    switchMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+  );
+
+
+  constructor(private http: HttpClient) {
+    // this.supllierWithConcatMap$.subscribe(item => console.log('concatMap result:', item));
+    // this.supllierWithMergeMap$.subscribe(item => console.log('MergeMap result:', item));
+    // this.supllierWithSwitchMap$.subscribe(item => console.log('Switch result:', item));
+  }
 
   private handleError(err: HttpErrorResponse): Observable<never> {
     // in a real world app, we may send the server to some remote logging infrastructure
--- sandbox/src/app/app.component.ts
+++ sandbox/src/app/app.component.ts
@@ -5,54 +5,37 @@ import {
   shareReplay,
   mergeMap,
   switchMapTo,
-  startWith
+  startWith,
+  of,
+  concatMap,
+  interval,
+  take,
+  fromEvent,
+  switchMap
 } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-import { AppService } from './app.service';
 @Component({
   selector: 'app-root',
   template: `
-
-  <div *ngIf="data$ | async as data; else loading">
-
-    <h1> {{data['first_name']}} </h1>
-    
-    <button (click)="update()">update</button>
-  </div>
-
-
-  <div *ngIf="data$ | async as data2">{{data2['email']}}</div>
-  
-  <ng-template #loading >
-    <div  style="width: 25px; height: 25px; margin: 15px; border-radius: 100%; border: solid 4px blue; border-left-color: transparent;"></div>
-    <small>... carregando</small>
-  </ng-template>
 
   `
 })
 export class AppComponent {
 
-  data$ = this.appService.loadData().pipe(startWith(null));
 
-  constructor(private appService: AppService) {
-  }
 
 
   ngOnInit() {
 
-    this.data$.subscribe(() => console.log('dalhe'));
+    const clicks = fromEvent(document, 'click');
 
-    console.log('teste')
+    const result = clicks.pipe(
+      switchMap(ev => interval(500).pipe(take(4)))
+    );
+    result.subscribe(x => console.log(x));
   }
 
-  update() {
 
-    this.appService.loadData();
-    this.appService.loadData();
-    this.appService.loadData();
-
-    this.data$ = this.appService.loadData();
-  }
 
 
 }
